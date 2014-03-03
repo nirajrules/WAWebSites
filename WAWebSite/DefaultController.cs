412ed87d1efe00b346641f047bb1253f694b1e98
nirajrules <niraj.rules@gmail.com>
2014-03-02 22:10:35 -0500

    Added MVC Controller

---
+++ WAWebSite/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WAWebSite
+{
+    public class DefaultController : Controller
+    {
+        //
+        // GET: /Default/
+        //public ActionResult Index()
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+            //return View();
+        }
+	}
+}
