a2cb459381cf889098d033f9d1d34aeed083b5f2
nirajrules <niraj.rules@gmail.com>
2014-03-02 22:27:41 -0500

    Added Trace

--- WAWebSite/DefaultController.cs
+++ WAWebSite/DefaultController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Diagnostics;
 
 namespace WAWebSite
 {
@@ -11,8 +12,9 @@ namespace WAWebSite
         //
         // GET: /Default/
         //public ActionResult Index()
-        public string Index()
+        public string Index()        
         {
+            Trace.WriteLine("Executing Index Method");
             return "This is my <b>default</b> action...";
             //return View();
         }
